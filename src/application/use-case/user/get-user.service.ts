85b5fa53483daa9f7f442c82e5fa51fcf352e434
Sarawuth Pimsai <sarawuth.pimsai@gmail.com>
2023-09-10 12:42:11 +0700

    Implement get user unit test

--- src/application/use-case/user/get-user.service.ts
+++ src/application/use-case/user/get-user.service.ts
@@ -1,8 +1,16 @@
+import GetUserRepository from '@application/port/repository/user/get-user.repository'
 import { User } from '@domain/entity/user'
 import GetUserUseCase from '@domain/use-case/user/get-user.use-case'
 
+export type GetUserServiceContext = {
+  getUserRepo: GetUserRepository
+}
 export default class GetUserService implements GetUserUseCase {
-  getUser(userId: string): Promise<User | undefined> {
+  private readonly getUserRepos: GetUserRepository
+  constructor(context: GetUserServiceContext) {
+    this.getUserRepos = context.getUserRepo
+  }
+  async getUser(userId: string): Promise<User | undefined> {
     throw new Error('Method not implemented.')
   }
 }
--- src/application/use-case/user/get-user.spec.ts
+++ src/application/use-case/user/get-user.spec.ts
@@ -1,3 +1,63 @@
+import { User } from '@domain/entity/user'
+import GetUserService, { GetUserServiceContext } from './get-user.service'
+import UserMemory, { UserMemoryConfig } from '@persistent/memory/user.memory'
+import { faker } from '@faker-js/faker'
+import InvalidParamsError from '@errors/invalid-params.error'
+import NotFoundError from '@errors/not-found.error'
+
 describe('Get User', () => {
-  it.todo('')
+  let users: User[]
+  function createUsers(no: number): User[] {
+    let users: User[] = []
+    return users
+  }
+
+  function createContext(users: User[]) {
+    const userMemoryConfig: UserMemoryConfig = {
+      users,
+    }
+    const userMemory: UserMemory = new UserMemory(userMemoryConfig)
+    const getUserServiceContext: GetUserServiceContext = {
+      getUserRepo: userMemory,
+    }
+    return { getUserServiceContext }
+  }
+  beforeAll(() => {
+    users = createUsers(30)
+  })
+  afterAll(() => {
+    users = []
+  })
+
+  it(`should return throw execption invalid user id`, () => {
+    const userId: string = faker.string.alpha({ length: { min: 10, max: 40 } })
+    const context = createContext(users)
+    const service: GetUserService = new GetUserService(
+      context.getUserServiceContext
+    )
+    expect(async () => {
+      await service.getUser(userId)
+    }).rejects.toThrow(InvalidParamsError)
+  })
+
+  it(`should return throw execption not found this user`, () => {
+    const userId: string = faker.string.uuid()
+    const context = createContext(users)
+    const service: GetUserService = new GetUserService(
+      context.getUserServiceContext
+    )
+    expect(async () => {
+      await service.getUser(userId)
+    }).rejects.toThrow(NotFoundError)
+  })
+
+  it(`should return success`, async () => {
+    const expectResult: User = faker.helpers.arrayElement<User>(users)
+    const context = createContext(users)
+    const service: GetUserService = new GetUserService(
+      context.getUserServiceContext
+    )
+    const result: User | undefined = await service.getUser(expectResult.id)
+    expect(result).toBe(expectResult)
+  })
 })
